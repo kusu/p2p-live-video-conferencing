7ae9d8690764d9ac2abbbf7434cf1a8ecdf1f1d9
pr kusu <pr.kusu@7e382477-152d-c520-6492-d35246db8c21>
2010-12-26 17:11:56 +0000

    some changes on UI structure

---
+++ P2PVideoConference/src/org/ioe/bct/p2pconference/ui/controls/ConferenceManager.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.ioe.bct.p2pconference.ui.controls;
+
+/**
+ *
+ * @author kusu
+ */
+public interface ConferenceManager {
+
+    public void startConference(ConferenceMode mode);
+    public void endConference();
+    public void joinConference();
+
+}
