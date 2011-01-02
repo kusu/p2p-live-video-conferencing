c663e7845a2d0a7570515c3f1d31c6827bdb9e65
pr kusu <pr.kusu@7e382477-152d-c520-6492-d35246db8c21>
2011-01-02 05:33:25 +0000

    threading managed

---
+++ P2PVideoConference/src/org/ioe/bct/p2pconference/dataobject/TextMessage.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.ioe.bct.p2pconference.dataobject;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author kusu
+ */
+public class TextMessage implements Serializable {
+    private String from;
+    private String message;
+    private String date;
+
+    public TextMessage() {
+
+    }
+
+    public TextMessage(String from,String msg,String date) {
+        this.from=from;
+        this.date=date;
+        this.message=msg;
+    }
+
+    public void setFrom(String frm) {
+        this.from=frm;
+    }
+
+    public void setMessage(String msg) {
+        this.message=msg;
+    }
+
+    public void setDate(String date) {
+        this.date=date;
+
+    }
+
+    public String getForm() {
+        return from;
+    }
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+}
